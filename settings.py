7b7954a4cc449d7d4886b4e926ba2b8cc35a124e
Liam Delahunty <Liam.Delahunty@croneri.co.uk>
2025-12-09 15:41:23 +0000

    Adjusted cache time.

--- settings.py
+++ settings.py
@@ -1,6 +1,12 @@
 # settings.py
 
-# Cache duration in seconds (e.g., 4 hours = 14400 seconds)
-CACHE_DURATION = 14400 
+# Cache duration in seconds 
+# (e.g., 
+# 1 hours = 3600 seconds,
+# 4 hours = 14400 seconds,
+# 1 day   = 86400 seconds,
+# 1 week  = 604800 seconds,
+# 1 month = 2419200 seconds)
+CACHE_DURATION = 604800 
 
 # Add other configurable settings here as needed.
